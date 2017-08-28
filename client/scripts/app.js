d73fd38cd572f987ccfa378865b7f9b2d09b5c18
Hack Reactor Student <student@hackreactor.com>
2017-08-28 17:04:22 -0500

    functioning app.js

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,7 @@ var Movie = Backbone.Model.extend({
   },
 
   toggleLike: function() {
-    // your code here
+    this.set('like', !this.get('like'));
   }
 
 });
@@ -16,12 +16,21 @@ var Movies = Backbone.Collection.extend({
 
   initialize: function() {
     // your code here
+    this.on('change', this.sort, this);
+    // this.model.on('change', function(){
+    //   this.sortByField(this.model.)
+    //   this.sort;
+    //
+    // },)
   },
 
   comparator: 'title',
 
   sortByField: function(field) {
-    // your code here
+    if( this.comparator !== field ) {
+      this.comparator = field;
+      this.sort();
+    };
   }
 
 });
@@ -58,7 +67,7 @@ var MovieView = Backbone.View.extend({
                         </div>'),
 
   initialize: function() {
-    // your code here
+    this.model.on('change', this.render, this);
   },
 
   events: {
@@ -66,7 +75,7 @@ var MovieView = Backbone.View.extend({
   },
 
   handleClick: function() {
-    // your code here
+    this.model.toggleLike();
   },
 
   render: function() {
@@ -79,7 +88,7 @@ var MovieView = Backbone.View.extend({
 var MoviesView = Backbone.View.extend({
 
   initialize: function() {
-    // your code here
+    this.collection.on('sort', this.render, this);
   },
 
   render: function() {
